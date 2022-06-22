6363e001ce54b48eef91a514bf1c4769b03e1124
Vitoria_Alves <vitoria.alves@dellteam.com>
2022-06-22 17:20:05 -0300

    Study on user data input manipulation with forms.

---
+++ formulario-desafio/src/components/NomeCompleto.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="nome-completo">
+    <Rotulo nome="Nome">
+		<input type="text" :value="value.nome" @input="alterarNome">
+	</Rotulo>
+	<Rotulo nome="Sobrenome">
+		<input type="text" :value="value.sobrenome" @input="alterarSobrenome">
+	</Rotulo>
+  </div>
+</template>
+
+<script>
+import Rotulo from './Rotulo.vue';
+
+export default {
+    name: 'NomeCompleto',
+    props: {
+        value: {
+            type: Object,
+            required: true
+        }
+    },
+    components: { Rotulo },
+    methods: {
+        alterarNome(event) {
+            this.$emit('input', {nome: event.target.value, sobrenome: this.value.sobrenome});
+        },
+        alterarSobrenome(event) {
+            this.$emit('input', {nome: this.value.nome, sobrenome: event.target.value});
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
